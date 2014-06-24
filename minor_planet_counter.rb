a5c85a01dc353a2dd0bcd299d1c363ce2989bd4e
Edward Paget <edward@cassetteta.pe>
2014-06-24 10:26:54 -0500

    Initial Commit

---
+++ minor_planet_counter.rb
@@ -0,0 +1,13 @@
+require 'nokogiri'
+require 'open-uri'
+
+class MinorPlanetCounter
+  def self.all_time_minor_planet_count
+    mpc = Nokogiri::HTML(open('http://minorplanetcenter.net/'))
+    mpc.css('.insetn .area:nth-child(3)').each do |link|
+      return link.content.gsub("\n","").gsub(/\s+/, "").split(":").last
+    end
+  end
+end
+
+p MinorPlanetCounter.all_time_minor_planet_count
