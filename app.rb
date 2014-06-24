70e910e3a6f1b14fc7dfb3e9bf700a1db37c1f37
Edward Paget <edward@cassetteta.pe>
2014-06-24 11:05:45 -0500

    Add CORS headers

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'json'
+require 'rack/cors'
 require_relative './minor_planet_counter'
 
 APP_ROOT = File.dirname(__FILE__)
@@ -7,9 +8,16 @@ APP_ROOT = File.dirname(__FILE__)
 class MPCFetcher < Sinatra::Application
   set :root, APP_ROOT
 
+  use Rack::Cors do
+    allow do
+      origins '*'
+      resource '/*', methods: [:get], headers: :all
+    end
+  end
+
   get '/count' do
     content_type "application/json"
-    MinorPlanetCounter.all_time_minor_planet_count.to_json
+    {count: MinorPlanetCounter.all_time_minor_planet_count}.to_json
   end
 
   get '/pingdom' do
