0b53b0f84ffb7dafdc84f7a8f02a741ce85471bb
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2014-07-05 10:47:03 -0500

    update planet counter for MPC site change

--- minor_planet_counter.rb
+++ minor_planet_counter.rb
@@ -4,12 +4,12 @@ require 'celluloid-http'
 class MinorPlanetCounter
   def self.all_time_minor_planet_count
     mpc = Nokogiri::HTML(html_page)
-    mpc.css('.insetn .area:nth-child(3)').each do |link|
-      return link.content.gsub("\n","").gsub(/\s+/, "").split(":").last
+    mpc.css('#main').each do |link|
+      return link.content.gsub("\n","").split("Orbits And Names").last.scan(/\b\d{6}\b/)[0]
     end
   end
 
   def self.html_page
-    Celluloid::Http.get('http://minorplanetcenter.net/').body
+    Celluloid::Http.get('http://www.minorplanetcenter.net/iau/lists/ArchiveStatistics.html').body
   end
 end
